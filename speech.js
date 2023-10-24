90ac07b288236e72b735666606626ae9174623bb
mariacarai <147046035+mariacarai@users.noreply.github.com>
2023-10-24 23:18:15 +0300

    Add files via upload

--- speech.js
+++ speech.js
@@ -1,18 +1,18 @@
 document.addEventListener("touchstart",on_touch);
 document.addEventListener("mousedown",on_touch);
-var recognition = new webkitSpeechRecognition();
-recognition.lang = 'ro-Ro';
+var recognition = new webkitSpeechRecognition(); 
+recognition.lang = 'en-US';
 function on_touch()
-{
-    if(recognition.start){
-    recognition.start();
-    recognition_started = true;
+    {
+        if(recognition.start){
+        recognition.start();
+        recognition_started = true;
+        }   
     }
-}
 function onend()
     {
-    recognition.stop();
-    recognition_started = false;
+        recognition.stop();
+        recognition_started = false;
     }
 recognition.onend = onend;
 recognition.onsoundend = onend;
@@ -20,5 +20,5 @@ recognition.onspeechend = onend;
 recognition.onresult =on_results;
 function on_results(e)
     {
-    document.getElementById("txt").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript + ", acuratete:"+e.results[0][0].confidence + "<br>" 
+        document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript + ", acuratete: "+e.results[0][0].confidence + "<br>" 
     }
